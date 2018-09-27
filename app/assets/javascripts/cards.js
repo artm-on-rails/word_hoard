01e66eb6e42f2f52c3f0c6740e7bd98d55b447a9
Artem Baguinski <abaguinski@gmail.com>
2018-09-27 19:06:08 +0200

    Add audio playback to cards#show

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,7 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on("turbolinks:load", function() {
-  $('.flip > *').click(function() {
+  $('.flip a').on("click", function(e) {
+    e.stopPropagation();
+  });
+
+  $('.flip > *').on("click", function() {
     $(this).toggleClass('flipped');
- });
+  });
+
+  $("[data-play]").on("click", function() {
+    $("#card_audio").get(0).play();
+  });
 });
