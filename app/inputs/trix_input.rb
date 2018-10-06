075eb7b8178d6c5594a53daec4ee68a9dc74b136
Artem Baguinski <abaguinski@gmail.com>
2018-10-06 17:57:08 +0200

    Don't require custom input id in the form

--- app/inputs/trix_input.rb
+++ app/inputs/trix_input.rb
@@ -6,13 +6,16 @@ class TrixInput < SimpleForm::Inputs::Base
       input_html_options,
       wrapper_options
     )
+    input_id = input_html_options.fetch(:id,
+      [object_name, attribute_name].join("_")
+    )
     template.capture do
       template.concat @builder.hidden_field(
         attribute_name,
         merged_input_options
       )
       template.concat template.content_tag("trix-editor", nil,
-        input: input_html_options[:id])
+        input: input_id)
     end
   end
 end
