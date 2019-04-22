8f20abb1d8829a10cc23b31298d8340ad39513ad
Artem Baguinski <abaguinski@gmail.com>
2019-04-22 14:43:51 +0200

    Old fixes

--- app/assets/javascripts/init-wavesurfer.js
+++ app/assets/javascripts/init-wavesurfer.js
@@ -1,5 +1,6 @@
 $(document).on("turbolinks:load", function() {
   var RegionsPlugin = WaveSurfer.regions;
+  var currentRegion = null;
 
   $("[data-wavesurfer]").each(function() {
     var wavesurfer = WaveSurfer.create({
@@ -13,11 +14,43 @@ $(document).on("turbolinks:load", function() {
     wavesurfer.load(url);
     wavesurfer.on("ready", function() {
       wavesurfer.zoom(50);
-      wavesurfer.enableDragSelection({});
     })
 
     $(this).find("a[data-toggle=play]").on("click", function() {
       wavesurfer.playPause();
-    })
+    });
+
+    $(this).find("a[data-toggle=play-loop]").on("click", function() {
+      playPauseLoop();
+    });
+
+    function playPauseLoop() {
+      if (!currentRegion) {
+        selectCurrentRegion();
+      }
+
+      if (!currentRegion) {
+        console.log("no current region")
+        return;
+      }
+
+      wavesurfer.playPause();
+    }
+
+    function selectCurrentRegion() {
+      if (wavesurfer.regions.list.length) {
+        console.log(wavesurfer.regions.list);
+        console.log("pick a region under cursor if possible");
+        return null;
+      }
+      console.log("add a region under cursor");
+      currentRegion = wavesurfer.addRegion({
+        start: 0,
+        end: 2.0,
+        loop: true,
+        drag: false,
+        resize: true
+      });
+    }
   });
 });
